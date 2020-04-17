4d49eb0f6ee3c2895e1096bea5f457d0adf159ee
yahe <yahesmile@126.com>
2020-04-17 16:30:11 +0800

    selectLabel

---
+++ src/components/SelectDemo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <label v-text="selectLabel"></label>
+        <select :name="name">
+            <option value="">--请选择--</option>
+            <option v-for="item in list" :value="item.value" :key="item.id">{{item.name}}</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectDemo',
+  props: {
+    selectLabel: String,
+    name: String,
+    list: Array
+  }
+}
+</script>
--- src/views/Page1.vue
+++ src/views/Page1.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="page1">
     <h1>This is an vue1 page</h1>
+    <div>
+      <SelectDemo :selectLabel="selectLabel" :list="list" :name="name"/>
+    </div>
   </div>
 </template>
+
+<script>
+import SelectDemo from '@/components/SelectDemo.vue'
+export default {
+  name: 'Page1',
+  data () {
+    return {
+      selectLabel: '自定义下拉label',
+      name: 'selectName',
+      list: [
+        { id: 1, value: 1, name: 'one' },
+        { id: 2, value: 2, name: 'two' }
+      ]
+    }
+  },
+  components: {
+    SelectDemo
+  }
+}
+</script>
